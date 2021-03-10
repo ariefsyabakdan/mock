8de387212e60ee82471c9106f7ef7c71a4fe5685
Cef Syarif <chefdeveloper29@gmail.com>
2021-03-10 20:29:06 +0700

    Menambah fungsi tampil ganjil dan tampil huruf

--- Soal 1/app.js
+++ Soal 1/app.js
@@ -1,10 +1,16 @@
 var angka = document.getElementById('angka')
 angka.addEventListener('keyup', checkInput, false)
+var elForm = document.getElementById('elForm')
+elForm.addEventListener('submit', cegahSubmit, false)
+var btnGanjil = document.getElementById('btnGanjil')
+btnGanjil.addEventListener('click', tampilGanjil,false)
+var btnHuruf = document.getElementById('btnHuruf')
+btnHuruf.addEventListener('click', tampilHuruf, false)
 
 function checkInput(){
   let inputAngka = parseInt(angka.value) 
   // cek input bukan huruf dan angka dari 0 - 4 
-  if(!Number.isInteger(inputAngka) && angka.value.length !== 0){
+  if((!Number.isInteger(inputAngka)) && angka.value.length > 0){
     window.alert('Harap masukan angka!')
     clearForm()
   }else if(inputAngka < 0){
@@ -18,4 +24,39 @@ function checkInput(){
 
 function clearForm(){
   document.getElementById('angka').value = ''
+}
+
+function cegahSubmit(e){
+  e.preventDefault()
+  clearForm()
+}
+
+function tampilGanjil(){
+  let inputAngka = parseInt(angka.value)
+  let angkaGanjil = [1,3,5,7,9,11,13,15,17,19]
+  let urutan = 0
+  let result = ''
+  for(let i = 1; i <= inputAngka; i++){
+    for(let j = 1; j <= i; j++){
+      result += `${angkaGanjil[urutan]} `
+      urutan += 1
+    }
+    result += '<br>'
+  }
+  document.getElementById('result').innerHTML = result
+}
+
+function tampilHuruf(){
+  let inputAngka = parseInt(angka.value)
+  let huruf = ['B','D','F','H','J','L','N','P','R','T']
+  let urutan = 0
+  let result = ''
+  for(let i = 1; i <= inputAngka; i++){
+    for(let j = 1; j <= i; j++){
+      result += `${huruf[urutan]} `
+      urutan += 1
+    }
+    result += '<br>'
+  }
+  document.getElementById('result').innerHTML = result
 }
